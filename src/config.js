688940b10d1020606c95e5d90130b913618eee0f
Miguel Gutierrez <miguel.gutierrez@aa.com>
2018-08-21 21:25:20 -0700

    deleting config file

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-	apiKey: "AIzaSyDGqvUz6xt3ZkDhHFk85rMIu7bQI_JLW4A",
-	authDomain: "journal-ed5a3.firebaseapp.com",
-	databaseURL: "https://journal-ed5a3.firebaseio.com",
-	projectId: "journal-ed5a3",
-	storageBucket: "journal-ed5a3.appspot.com",
-	messagingSenderId: "295577169635"
-};
